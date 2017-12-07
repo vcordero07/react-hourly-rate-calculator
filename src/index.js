f681a338c9d4cd44b0500e9ccc59600604f9faf5
vcordero07 <vcordero07@gmail.com>
2017-12-06 23:33:50 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RateCalculator from './components/rate-calculator';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <RateCalculator />,
+  document.getElementById('root')
+);
